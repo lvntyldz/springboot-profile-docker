93235ada613aee248df2c578aa77fb161582e0b7
levent yıldız <lewenty@gmail.com>
2020-04-01 17:36:18 +0300

    initialize commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/get-info")
+public class HomeController {
+
+    @GetMapping
+    public String getEnvironmentInfo(){
+        return "Hello World.";
+    }
+
+}
